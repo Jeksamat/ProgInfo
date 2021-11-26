27eeac40de0e9cc4184039fe6fd75c55b6c39793
Jeksamat <45683029+Jeksamat@users.noreply.github.com>
2021-11-26 16:20:18 +0100

    Update Drill8.cpp
    Finished ver

--- CH10/Drill8.cpp
+++ CH10/Drill8.cpp
@@ -8,6 +8,7 @@ struct Point {
 //1 2 13 4 5.1 6 7,8 (9 10) 1 1 2 2
 int main() 
 try {
+//Original_points
 	//containers
 	vector<Point> original_points;
 	vector<double> Xs;
@@ -17,10 +18,10 @@ try {
 	//promt
 	cout << "Enter 7 (x,y) pairs:" << endl;
 	//READ
-	while (Xs.size() < 7 || Ys.size() < 7) { //no escape
+	while (Ys.size() < 7) { //no escape
 		cin >> n;
 		if (cin) {
-			cout << n << ' ';//self check
+			//cout << n << ' ';//self check
 			if (Xs.size() > Ys.size())
 				Ys.push_back(n);
 			else
@@ -38,13 +39,42 @@ try {
 		else {
 			error("No input");
 		}
-	}	
-	//WRITE
+	}
 	for (int i = 0; i < Xs.size(); i++) {
 		original_points.push_back(Point{ Xs[i],Ys[i] });
 	}
+	//WRITE
+		//to console
 	for (const auto& R : original_points) {
-		cout << '(' << R.X << ',' << R.Y << ')' << endl;
+		cout << R.X << ' ' << R.Y << endl;
+	}
+		//to file
+	ofstream ost{"mydata.txt"};
+	if (!ost)
+		error("Can't open mydata.txt");
+	for (const auto& R : original_points) {
+		ost << R.X << ' ' << R.Y << endl;
+	}
+//Processed_points
+	//containers
+	vector<Point> processed_points;
+	ifstream ist{ "mydata.txt" };
+	//variables
+	double ix, iy;
+	//READ
+	while (ist >> ix >> iy) {
+		processed_points.push_back(Point{ ix,iy });
+	}
+	//WRITE
+	cout << endl;
+	for (const auto& R : processed_points) {
+		cout << R.X << ' ' << R.Y << endl;
+	}
+//COMPARE
+
+	for (int i = 0; i < original_points.size(); i++) {
+		if (original_points[i].X != processed_points[i].X)
+			error("Something's wrong!");
 	}
 
 	return 0;
